d32a337ab3a2efd646d2a3185dde0edac3c10e36
GuilleF <guilleferrer@gmail.com>
2024-06-17 16:06:51 +0200

    Adds shortcut bin/replace develop or bin/replace production

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git add src 
+# git get current branch and store it in a variable
+current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+# replace_url.sh script is executed with the first argument as current branch
+bin/replace_url.sh $current_branch
+
+git add src
 git ci -am "Updates static"
 echo "Committed static"
 # set current branch in a variable and then push against it
--- bin/replace_url.sh
+++ bin/replace_url.sh
@@ -4,19 +4,27 @@
 DEFAULT_OLD_URL="static-www-vcards.tictap.me"
 DEFAULT_NEW_URL="vcards.tictap.me"
 
+# If first argument is "develop" then set the default values to the develop environment
+if [ "$1" == "develop" ]; then
+  DEFAULT_OLD_URL="static-www-vcards\\.tictap\\.me"
+  DEFAULT_NEW_URL="develop.vcards-web.pages.dev"
+fi
+
+if [ "$1" == "main" ]; then
+  DEFAULT_OLD_URL="static-www-vcards\\.tictap\\.me|develop\\.vcards-web\\.pages\\.dev"
+  DEFAULT_NEW_URL="vcards.tictap.me"
+fi
+
 # Check if the correct number of arguments are provided
 if [ "$#" -lt 1 ] || [ "$#" -gt 3 ]; then
-  echo "Usage: $0 src_dir [old_url] [new_url]"
+  echo "Usage: $0 [old_url] [new_url]"
   exit 1
 fi
 
 # Assign arguments to variables
-SRC_DIR="$1"
+SRC_DIR="src/"
 OLD_URL="${2:-$DEFAULT_OLD_URL}"
 NEW_URL="${3:-$DEFAULT_NEW_URL}"
-
-
-SRC_DIR="$1"
 
 # Check if the provided directory exists
 if [ ! -d "$SRC_DIR" ]; then
@@ -27,6 +35,8 @@ fi
 # Find all files in the src directory and process them
 find "$SRC_DIR" -type f | while read -r file; do
   # Use sed to replace the URL and save changes in place
-  sed -i "s|$OLD_URL|$NEW_URL|g" "$file"
+  sed -i -E "s/$OLD_URL/$NEW_URL/g" "$file"
   echo "Processed $file"
 done
+
+echo "Replaced URLs from $OLD_URL to $NEW_URL in $SRC_DIR"
