9a235afb8a11526e6c957a2cc86e0e097489a082
GuilleF <guilleferrer@gmail.com>
2026-01-29 14:31:23 +0100

    Adds ai.md detailing TicTAP's digital business card solution

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -avz --exclude='.git/' --exclude='404.html' --exclude='bin/' --exclude="_redirects" debian@51.83.111.98:/home/debian/docker-wp/generated_vcards/ src/ --delete
+rsync -avz --exclude='.git/' --exclude='404.html' --exclude='bin/' --exclude="_redirects" --exclude="src/ai.md" debian@51.83.111.98:/home/debian/docker-wp/generated_vcards/ src/ --delete
 # replace using sed all staging links with production links
 find ./src/ -name "*.html" -type f -print0 | xargs -0 sed -i 's/staging-www\.tictap\.me/www\.tictapcards\.com/g'
 find ./src/ -name "*.html" -type f -print0 | xargs -0 sed -i 's/staging-vcards-www\.tictap\.me/www\.tictapcards\.com/g'
